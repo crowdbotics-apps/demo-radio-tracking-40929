50e14c6676a36e9817a4e40b7c54fe0d4abb7fc6
Crowdbotics <team@crowdbotics.com>
2023-05-23 16:43:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "demo-radio-tracking-40929",
+  url: "https://demo-radio-tracking-40929.botics.co",
+  api: "https://demo-radio-tracking-40929.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
